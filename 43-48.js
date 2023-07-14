b17fc123337d50e90066fa7b1d5471eb3e3d4c9b
Rabiya <rabiyaimt786@gmail.com>
2023-07-14 22:23:16 +0500

    add files

---
+++ 43-48.js
@@ -0,0 +1,51 @@
+// /////question no 1//////////
+/////////////on click event/////////
+
+
+///////question no 2////////
+// ////////on click event add on images///////
+
+
+// ////question no 3//////
+// function SomeDeleteRowFunction(o) {
+  
+//   let p=o.parentNode.parentNode;
+//       p.parentNode.removeChild(p);
+//  }
+
+
+
+// /////question no 4///////
+
+// ///////change image////////
+
+
+// ////question no 5//////
+// Select increment and decrement buttons
+// const incrementCount = document.getElementById("increment-count");
+// const decrementCount = document.getElementById("decrement-count");
+
+// // Select total count
+// const totalCount = document.getElementById("total-count");
+
+// // Variable to track count
+// var count = 0;
+
+// // Display initial count value
+// totalCount.innerHTML = count;
+
+// // Function to increment count
+// const handleIncrement = () => {
+//   count++;
+//   totalCount.innerHTML = count;
+// };
+
+// // Function to decrement count
+// const handleDecrement = () => {
+//   count--;
+//   totalCount.innerHTML = count;
+// };
+
+// // Add click event to buttons
+// incrementCount.addEventListener("click", handleIncrement);
+// decrementCount.addEventListener("click", handleDecrement);
---
+++ 49-52.js
@@ -0,0 +1,16 @@
+// /////////question no 1//////////
+
+        
+        //   function displayFormData() {
+        //     var name = document.getElementById('name').value;
+        //     var email = document.getElementById('email').value;
+        //     var password = document.getElementById('password').value;
+            
+        //     var displayDiv = document.getElementById('display-data');
+            
+        //     var formData = "<p><strong>Name:</strong> " + name + "</p>" +
+        //                    "<p><strong>Email:</strong> " + email + "</p>" +
+        //                    "<p><strong>Password:</strong> " + password + "</p>";
+            
+        //     displayDiv.innerHTML = formData;
+        // }
